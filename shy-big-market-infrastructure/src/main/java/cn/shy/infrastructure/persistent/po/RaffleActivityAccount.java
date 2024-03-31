c439020b345a26439700d27cc67dd60f67603a2e
shy <3064267442@qq.com>
2024-03-31 20:42:53 +0800

    feat: 用户参与抽奖活动库表设计

---
+++ shy-big-market-infrastructure/src/main/java/cn/shy/infrastructure/persistent/po/RaffleActivityAccount.java
@@ -0,0 +1,70 @@
+package cn.shy.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 抽奖活动账户表
+ * @author shy
+ * @since 2024/3/31 17:44
+ */
+@Data
+public class RaffleActivityAccount {
+    
+    /**
+     * 自增ID
+     */
+    private Long id;
+    
+    /**
+     * 用户ID
+     */
+    private String userId;
+    
+    /**
+     * 活动ID
+     */
+    private Long activityId;
+    
+    /**
+     * 总次数
+     */
+    private Integer totalCount;
+    
+    /**
+     * 总次数-剩余
+     */
+    private Integer totalCountSurplus;
+    
+    /**
+     * 日次数
+     */
+    private Integer dayCount;
+    
+    /**
+     * 日次数-剩余
+     */
+    private Integer dayCountSurplus;
+    
+    /**
+     * 月次数
+     */
+    private Integer monthCount;
+    
+    /**
+     * 月次数-剩余
+     */
+    private Integer monthCountSurplus;
+    
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+    
+}
